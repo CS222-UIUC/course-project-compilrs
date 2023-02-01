c54e4bf9c2c5ff4dc84ada23ffb07497be51ffd8
Ayush Raman <54257595+ayuram@users.noreply.github.com>
2023-02-01 13:30:47 -0600

    Initial Commit

---
+++ addez/addezApp.swift
@@ -0,0 +1,17 @@
+//
+//  addezApp.swift
+//  addez
+//
+//  Created by Ayush Raman on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct addezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ addezUITests/addezUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  addezUITestsLaunchTests.swift
+//  addezUITests
+//
+//  Created by Ayush Raman on 2/1/23.
+//
+
+import XCTest
+
+final class addezUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
