c56ffd17c08d3c9348301a8ce6ddd15dc7c4ec3c
ssadler22 <spencersayshi22@gmail.com>
2023-02-23 14:32:58 -0600

    Finished getDeterminant

---
+++ addez/models/Tools.swift
@@ -0,0 +1,26 @@
+//
+//  Tools.swift
+//  addez
+//
+//  Created by Spencer Sadler on 2/23/23.
+//
+
+import Foundation
+
+extension Int {
+    func toDouble() -> Double {
+        Double(self)
+    }
+}
+
+extension Double {
+    func toInt() -> Int {
+        Int(self)
+    }
+}
+
+extension Decimal {
+    func toDouble() -> Double {
+        Double(self)
+    }
+}
