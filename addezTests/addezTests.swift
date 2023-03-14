a722978ad5ee5e8742a25c7117886e376298c036
Sanjay Manoj <sanjaymanoj04@gmail.com>
2023-03-14 18:27:21 -0500

    Update addezTests.swift

--- addezTests/addezTests.swift
+++ addezTests/addezTests.swift
@@ -6,41 +6,22 @@
 //
 
 import XCTest
+@testable import addez
 
 final class addezTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testRowEchelon2x2() {
+        let matrix = [[1.0, 3.0], [4.0, 9.0]]
+        let return_matrix = rowEchelon(matrix: matrix)
+        let expected = [[1.0, 2.25], [0.0, 1.0]]
+        XCTAssertEqual(return_matrix, expected)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-        
-        // create a matrix of size 2x2 with [[1, 3], [4, 9]] and apply the row reduction algorithm to it
-        // create a matrix of size 3x3 with [[1, 3, 5], [4, 9, 2], [7, 6, 8]] and apply the row reduction algorithm to it
-        // create a matrix of size 4x4 with [[1, 3, 5, 7], [4, 9, 2, 6], [7, 6, 8, 4], [3, 2, 1, 5]] and apply the row reduction algorithm to it
-
-        matrix = [[1, 3], [4, 9]]
-        // apply the row reduction algorithm to the matrix
-        // check if the matrix is in reduced row echelon form
-        return_matrix = rowEchelon(matrix: matrix)
-        // print out return matrix
-        print(return_matrix)
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testRowEchelon3x3() {
+        let matrix = [[1.0, 2.0, 6.0], [4.0, 10.0, 6.0], [5.0, 8.0, 12.0]]
+        let returny = rowEchelon(matrix: matrix)
+        let expected = [[1.0, 1.6, 2.4], [0.0, 1.0, -1.0], [0.0, 0.0, 1.0]]
+        XCTAssertEqual(returny, expected)
     }
 
 }
