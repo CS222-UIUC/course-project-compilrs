b2ae728043fb133323896602f37bdfcdedd752db
Ayush Raman <54257595+ayuram@users.noreply.github.com>
2023-04-19 22:20:53 -0500

    landing page changed

--- addez/views/LandingView.swift
+++ addez/views/LandingView.swift
@@ -10,11 +10,25 @@ import SwiftUI
 struct LandingView: View {
     var body: some View {
         NavigationView {
-            List {
-                Text("Matrices")
-                    .navLink(MatrixSolveView())
-                Text("Integrals")
-                    .navLink(IntegralSolveView())
+            ScrollView {
+                GeometryReader { geometry in
+                    Text("Matrices")
+                        .cardView(.canvas)
+                        .navLink(MatrixSolveView())
+                        .padding(15)
+                        .shadow(radius: 10)
+                        .rotation3DEffect(Angle(degrees: (geometry.frame(in: .global).minY - 10) / 50.0), axis: (x: 10.0, y: 0.0, z: 0))
+                }
+                .frame(height: 300)
+                GeometryReader { geometry in
+                    Text("Integrals")
+                        .cardView(.canvas)
+                        .navLink(IntegralSolveView())
+                        .padding(15)
+                        .shadow(radius: 10)
+                        .rotation3DEffect(Angle(degrees: (geometry.frame(in: .global).minY - 10) / 50.0), axis: (x: 10.0, y: 0.0, z: 0))
+                }
+                .frame(height: 300)
             }
         }
         .navigationBarTitle("Home")
